9acfee3bd5d309b948636d1437502cbbd113aa11
Michel Mattos <mattos.michel@gmail.com>
2018-05-03 19:05:59 +1200

    Updated coding style with prettier

--- index.test.js
+++ index.test.js
@@ -1,59 +1,59 @@
-"use strict";
-const fs = require("fs");
-const $ = require("jquery");
+'use strict';
+const fs = require('fs');
+const $ = require('jquery');
 
-test("it passes", () => {
-  expect(true).toBe(true);
+test('it passes', () => {
+    expect(true).toBe(true);
 });
 
-describe("Email must be a valid email address", () => {
-  test("input type is email", () => {
-    renderHtml("index.html");
-    expect($("#email").attr("type")).toBe("email");
-  });
+describe('Email must be a valid email address', () => {
+    test('input type is email', () => {
+        renderHtml('index.html');
+        expect($('#email').attr('type')).toBe('email');
+    });
 
-  test("passes when a valid email is passed", () => {
-    renderHtml("index.html");
-    $("#email").val("john.doe@example.com");
-    expect($("#email")[0].checkValidity()).toBe(true);
-  });
+    test('passes when a valid email is passed', () => {
+        renderHtml('index.html');
+        $('#email').val('john.doe@example.com');
+        expect($('#email')[0].checkValidity()).toBe(true);
+    });
 
-  test("do not pass when a non valid email is passed", () => {
-    renderHtml("index.html");
-    $("#email").val("john.doe");
-    expect($("#email")[0].checkValidity()).toBe(false);
-  });
+    test('do not pass when a non valid email is passed', () => {
+        renderHtml('index.html');
+        $('#email').val('john.doe');
+        expect($('#email')[0].checkValidity()).toBe(false);
+    });
 });
 
-describe("Password must be longer than 8 characters", () => {
-  test("passes when password is greater than 8 characters", () => {
-    renderHtml("index.html");
-    $("#password").val("123456789");
-    expect($("#password")[0].checkValidity()).toBe(true);
-  });
+describe('Password must be longer than 8 characters', () => {
+    test('passes when password is greater than 8 characters', () => {
+        renderHtml('index.html');
+        $('#password').val('123456789');
+        expect($('#password')[0].checkValidity()).toBe(true);
+    });
 
-  test("do not pass when password is less or equal than 8 characters", () => {
-    renderHtml("index.html");
-    $("#password").val("12345678");
-    expect($("#password")[0].checkValidity()).toBe(false);
-  });
+    test('do not pass when password is less or equal than 8 characters', () => {
+        renderHtml('index.html');
+        $('#password').val('12345678');
+        expect($('#password')[0].checkValidity()).toBe(false);
+    });
 });
 
-describe("Colour must be selected", () => {
-  test("passes when a colour is selected", () => {
-    renderHtml("index.html");
-    $("#colour").val("blue");
-    expect($("#colour")[0].checkValidity()).toBe(true);
-  });
+describe('Colour must be selected', () => {
+    test('passes when a colour is selected', () => {
+        renderHtml('index.html');
+        $('#colour').val('blue');
+        expect($('#colour')[0].checkValidity()).toBe(true);
+    });
 
-  test("do not pass when a colour is not selected", () => {
-    renderHtml("index.html");
-    $("#colour").val("");
-    expect($("#colour")[0].checkValidity()).toBe(false);
-  });
+    test('do not pass when a colour is not selected', () => {
+        renderHtml('index.html');
+        $('#colour').val('');
+        expect($('#colour')[0].checkValidity()).toBe(false);
+    });
 });
 
 function renderHtml(filepath) {
-  const html = fs.readFileSync(filepath, { encoding: "utf-8" });
-  $("html").html(html);
+    const html = fs.readFileSync(filepath, { encoding: 'utf-8' });
+    $('html').html(html);
 }
